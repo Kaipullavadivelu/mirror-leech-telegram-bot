fb032c8bda28dc06a44d299aa453accc3d71534a
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-12-05 22:11:48 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -23,8 +23,10 @@ def cloneNode(update, context):
         link = ''
     gdtot_link = is_gdtot_link(link)
     if gdtot_link:
+        msg0 = sendMessage('Connecting to GDToT.....', context.bot, update)
         try:
             link = gdtot(link)
+            deleteMessage(context.bot, msg0)
         except DirectDownloadLinkException as e:
             return sendMessage(str(e), context.bot, update)
     if is_gdrive_link(link):
